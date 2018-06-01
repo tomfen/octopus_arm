286c7611c82face13286abf0e8c8d73d319e020e
tomfen <tomfen999@gmail.com>
2018-06-01 15:16:40 +0200

    script for training/testing

---
+++ TRAIN.py
@@ -0,0 +1,40 @@
+import glob
+import os
+from subprocess import Popen
+
+import time
+
+from agent_handler import Handler
+import numpy as np
+
+
+def calculate_score(log_file_path):
+    with open(log_file_path) as file:
+        score = sum(float(x) for x in file.read().split())
+    return score
+
+for log_file in glob.iglob('*.log'):
+    os.remove(log_file)
+
+scores = []
+
+tests = glob.glob(os.path.join('tests', '*.xml'))
+tests = sorted(tests, key=lambda s: s[-7:-5])
+
+for test in tests:
+    print(test, end=': ')
+    server_command = 'javaw -jar ./octopus-environment.jar external_gui %s 7777' % test
+    with Popen(server_command) as proc:
+        Handler().run(['localhost', '7777', '1'])
+        proc.kill()
+
+        log_file = glob.glob('*.log')[0]
+        score = calculate_score(log_file)
+
+        scores.append(score)
+        print(score)
+
+        time.sleep(0.1)  # HACK
+        os.remove(log_file)
+
+print('mean: %.2f' % np.mean(scores))
---
+++ agent_handler.py
@@ -0,0 +1,88 @@
+# Echo client program
+import socket
+import sys
+import random
+import Agent
+
+LINE_SEPARATOR = b'\n'
+BUF_SIZE = 4096  # in bytes
+
+
+class Handler:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def getArgs(self, args):
+        try:
+            return args[0], int(args[1]), int(args[2]), 'sample_agents/weights.txt'
+        except ValueError:
+            print('usage: python agent_handler <server> <port> <num-episodes> [<agent-specific parameters>]')
+            sys.exit(1)
+
+    def connect(self, host, port):
+        try:
+            self.sock.connect((host, port))
+        except socket.error:
+            print('Unable to contact environment at the given host/port.')
+            sys.exit(1)
+
+
+    def sendStr(self, s):
+        self.sock.send(s.encode() + LINE_SEPARATOR)
+
+    def receive(self, numTokens):
+        data = [b'']
+        while len(data) <= numTokens:
+            rawData = data[-1] + self.sock.recv(BUF_SIZE)
+            del data[-1]
+            data = data + rawData.split(LINE_SEPARATOR)
+
+        del data[-1]
+        return data
+
+    def sendAction(self, action):
+        # sends all the components of the action one by one
+        for a in action:
+            self.sendStr(str(a).replace('.', ',', 1))
+
+    def run(self, args):
+
+        host, port, numEpisodes, agentParams = self.getArgs(args)
+
+        self.connect(host, port)
+
+        self.sendStr('GET_TASK')
+        data = self.receive(2)
+        stateDim = int(data[0])
+        actionDim = int(data[1])
+
+        # instantiate agent
+        agent = Agent.Agent(stateDim, actionDim, agentParams)
+
+        self.sendStr('START_LOG')
+        self.sendStr(agent.getName())
+
+        for i in range(numEpisodes):
+            self.sendStr('START')
+            data = self.receive(2 + stateDim)
+
+            terminalFlag = int(data[0])
+            state = [float(x) for x in data[2:]] #map(float, data[2:])
+            action = agent.start(state)
+
+            while not terminalFlag:
+                self.sendStr('STEP')
+                self.sendStr(str(actionDim))
+                self.sendAction(action)
+
+                data = self.receive(3 + stateDim)
+                if not (len(data) == stateDim + 3):
+                    print('Communication error: calling agent.cleanup()')
+                    agent.cleanup()
+                    sys.exit(1)
+
+                reward = float(data[0])
+                terminalFlag = int(data[1])
+                state = [float(x) for x in data[3:]] #map(float, data[3:])
+
+                action = agent.step(reward, state)
